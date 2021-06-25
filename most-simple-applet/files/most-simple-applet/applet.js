2b9703a89b0a938fbae8065e912443d77c5e5156
jonath92 <jonathanheard92@gmail.com>
2021-06-25 17:22:35 +0200

    workinggit add .!

--- most-simple-applet/files/most-simple-applet/applet.js
+++ most-simple-applet/files/most-simple-applet/applet.js
@@ -1,10 +1,16 @@
 const Applet = imports.ui.applet;
 const Main = imports.ui.main;
+const { webpackNumbers } = require('./webpack-numbers');
 
 class MyApplet extends Applet.IconApplet {
     constructor(orientation, panel_height, instance_id) {
         super(orientation, panel_height, instance_id)
         this.set_applet_icon_name("computer");
+    }
+
+    on_applet_clicked = function () {
+        global.log(webpackNumbers.wordToNum('Two'))
+
     }
 }
 
