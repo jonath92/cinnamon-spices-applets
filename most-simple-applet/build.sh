de1ec01e04cb27b800313c1ab6942ffa0145c392
jonath92 <jonathanheard92@gmail.com>
2021-06-27 18:03:06 +0200

    build improvement

--- most-simple-applet/build.sh
+++ most-simple-applet/build.sh
@@ -3,8 +3,8 @@
 CURRENT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 APPLET_NAME=$(basename $CURRENT_DIR)
-LOCAL_TESTING_DIR=$HOME/.local/share/cinnamon/applets/${APPLET_NAME}/${CINNAMON_VERSION}
-BUILD_DIR=${CURRENT_DIR}/files/${APPLET_NAME}/${CINNAMON_VERSION}
+LOCAL_TESTING_DIR=$HOME/.local/share/cinnamon/applets/${APPLET_NAME}
+BUILD_DIR=${CURRENT_DIR}/files/${APPLET_NAME}
 
 npx webpack
 # when webpack succeeded. Must be directly behind the command
