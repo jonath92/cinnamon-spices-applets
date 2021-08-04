0aea65d96c0c1f513934a1a24df7c2b4cf0674ee
jonath92 <jonathanheard92@gmail.com>
2021-08-04 20:08:10 +0200

    initial commit

---
+++ reminder@jonath92/src/index.ts
@@ -0,0 +1,32 @@
+const Applet = imports.ui.applet;
+const Main = imports.ui.main;
+
+
+class MyApplet extends Applet.IconApplet {
+    constructor(orientation: imports.gi.St.Side, panel_height: number, instance_id: number) {
+        super(orientation, panel_height, instance_id)
+        this.set_applet_icon_name("computer");
+    }
+
+    on_applet_clicked() {
+        global.log('applet clicked')
+    }
+}
+
+interface Arguments {
+    orientation: imports.gi.St.Side,
+    panelHeight: number,
+    instanceId: number
+}
+
+
+function main(args: Arguments) {
+
+    const {
+        orientation,
+        panelHeight: panel_height,
+        instanceId: instance_id
+    } = args
+
+    return new MyApplet(orientation, panel_height, instance_id)
+}
---
+++ reminder@jonath92/webpack.config.js
@@ -0,0 +1,43 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs')
+
+const cinnamonVersion = '4.6'
+const appletFullName = __dirname.split('/').slice(-1)[0]
+const appletShortName = appletFullName.split('@')[0]
+
+console.log(appletShortName)
+
+// could both also be any other name
+const bundledFileName = `${appletShortName}-applet.js`
+const libraryName = `${appletShortName}Applet`
+
+// TODO: write the content of applet.js to file
+
+const buildPath = path.resolve(__dirname, `files/${appletFullName}/${cinnamonVersion}/`)
+
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    devtool: "eval-source-map",
+    target: 'node', // without webpack renames 'global'
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: buildPath,
+        filename: bundledFileName,
+        library: libraryName,
+    },
+};
