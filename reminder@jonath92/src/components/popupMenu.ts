6b616e678fea68b8f006e4e8e1ae179129d25d99
jonath92 <jonathanheard92@gmail.com>
2021-10-09 14:10:01 +0200

    calendar and cards shown

--- reminder@jonath92/src/components/popupMenu.ts
+++ reminder@jonath92/src/components/popupMenu.ts
@@ -5,7 +5,7 @@ import { createCalendar } from "./Calendar";
 import { createCard } from "./Card";
 import { createCardContainer } from "./CardContainer";
 
-const { Button } = imports.gi.St
+const { BoxLayout, Bin } = imports.gi.St
 
 interface Arguments {
     launcher: imports.gi.St.BoxLayout
@@ -20,8 +20,17 @@ export function createCalendarPopupMenu(args: Arguments) {
     const popupMenu = createPopupMenu({ launcher })
 
     const cardContainer = createCardContainer()
+    const calendar = createCalendar()
 
-    popupMenu.add_actor(cardContainer.actor)
+    const container = new BoxLayout()
+
+    container.add_child(cardContainer.actor)
+
+    const calendarContainer = new Bin({child: calendar, x_expand: false, y_expand: false})
+
+    container.add_child(calendarContainer)
+
+    popupMenu.add_child(container)
 
    // popupMenu.set_style_class_name('calendar-background')
 
@@ -44,8 +53,7 @@ export function createCalendarPopupMenu(args: Arguments) {
     }
 
 
-    const calendar = createCalendar()
-    popupMenu.add_child(calendar)
+
 
 
     return {
