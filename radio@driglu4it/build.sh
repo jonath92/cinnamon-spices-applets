2e5bc61b1f0afaf18442e2b915e1370a2261e439
jonathanheard <jonathan.heard@prodyna.com>
2022-04-10 12:07:39 +0200

    finalized build script

--- radio@driglu4it/build.sh
+++ radio@driglu4it/build.sh
@@ -2,10 +2,14 @@
 CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 UUID="$(basename "$CURRENT_DIR")"
 FILES_DIR=${CURRENT_DIR}/files/${UUID}
-
 VERSION=4.6
+OUTPUT_DIR=${FILES_DIR}/${VERSION}
 
-rm -rf 
-ln -s ${FILES_DIR}/icons ${FILES_DIR}/${VERSION}
+rm -rf ${OUTPUT_DIR} 
+mkdir ${OUTPUT_DIR}
+ln -s ${FILES_DIR}/icons ${OUTPUT_DIR}
 
-npx lerna run build && cinnamon-install-spice applet && xdotool key ctrl+alt+0xff1b
+cp ${CURRENT_DIR}/assets/* ${OUTPUT_DIR}
+
+
+# npx lerna run build && cinnamon-install-spice applet && xdotool key ctrl+alt+0xff1b
