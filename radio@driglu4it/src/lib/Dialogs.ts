1550b1a6789b20872345f71d4f8db9dd9ba9195c
jonathanheard <jonathan.heard@prodyna.com>
2022-04-18 13:46:16 +0200

    current

--- radio@driglu4it/src/lib/Dialogs.ts
+++ radio@driglu4it/src/lib/Dialogs.ts
@@ -1,40 +1,36 @@
+const { Widget, Bin, BoxLayout, Align, Label } = imports.gi.St;
+const { Role } = imports.gi.Atk;
+const { uiGroup } = imports.ui.main;
+const { BindConstraint, BindCoordinate, Group } = imports.gi.Clutter;
+const { Lightbox } = imports.ui.lightbox;
+const { Stack } = imports.gi.Cinnamon;
+const { State } = imports.ui.modalDialog;
 
-const { Widget, Bin, BoxLayout} = imports.gi.St
-const { Role } = imports.gi.Atk
-const { uiGroup } = imports.ui.main
-const { BindConstraint, BindCoordinate } = imports.gi.Clutter
+interface ModalDialogParams {
+  /**  whether the modal dialog should block Cinnamon input */
+  cinnamonReactive: boolean;
+  styleClass: string;
+}
+class ModalDialog {
+  public state: number;
+  private _hasModal: boolean;
+  private _cinnamonReactive: boolean;
+  private _group: imports.gi.St.Widget;
 
-const Lightbox = imports.ui.lightbox;
+  constructor(params?: Partial<ModalDialogParams>) {
+    this.state = State.CLOSED;
+    this._hasModal = false;
 
-export function createBaseDialog() {
+    this._cinnamonReactive = params?.cinnamonReactive || false;
+    this._group = new Widget({
+      visible: false,
+      x: 0,
+      y: 0,
+      accessible_role: Role.DIALOG,
+    });
 
-    const group = new Widget({
-        visible: false,
-        x: 0,
-        y: 0,
-        accessible_role: Role.DIALOG
-    })
+    uiGroup.add_actor(this._group)
 
-    uiGroup.add_child(group)
-
-    const constraint = new BindConstraint({
-        source: global.stage,
-        coordinate: BindCoordinate.POSITION | BindCoordinate.SIZE
-    })
-
-    group.add_constraint(constraint)
-
-    const backgroundBin = new Bin()
-    group.add_child(backgroundBin)
-
-    group.add_child(backgroundBin)
-
-    const dialogLayout = new BoxLayout({
-        style_class: 'modal-dialog', 
-        vertical: true
-    })
-
-    const lightBox = 
-
-
+    
+  }
 }
