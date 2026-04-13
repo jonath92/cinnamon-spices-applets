53884f16188db53833a01b298213b0af65c8cc06
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2026-04-13 14:14:59 +0200

    revert script change

--- radio@driglu4it/files/radio@driglu4it/6.4/mpv-reconnect.lua
+++ radio@driglu4it/files/radio@driglu4it/6.4/mpv-reconnect.lua
@@ -1,23 +1,11 @@
--- Util to automatically reconnect on network change or temporary loss.
--- Based on: https://github.com/mpv-player/mpv/issues/5793#issuecomment-2495268210
--- When mpv goes idle after a stream error, 'path' becomes nil, so we
--- remember the last URL so the reload still works.
+-- Util to automatically reconnect on network change. 
+-- Based on: https://github.com/mpv-player/mpv/issues/5793#issuecomment-2495268210 
 local prev_pos = -1
-local saved_url = nil
-
-mp.observe_property('path', 'string', function(name, value)
-  if value then
-    saved_url = value
-  end
-end)
 
 function reload_if_disconnected()
   local pos = mp.get_property_number('time-pos')
   if pos == nil or pos == prev_pos then
-    local url = mp.get_property('path') or saved_url
-    if url then
-      mp.commandv('loadfile', url, 'replace')
-    end
+    mp.commandv('loadfile', mp.get_property('path'), 'replace')
   else
     prev_pos = pos
   end
